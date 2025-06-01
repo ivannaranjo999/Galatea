7309f13597932e0b20f065a908938a888ffccce8
ivannaranjo999 <ivanlink99@gmail.com>
2025-06-01 23:28:03 +0200

    Add priority, deadline and edit verbs

--- todo.c
+++ todo.c
@@ -21,12 +21,20 @@ int load_todos(Todo todos[]) {
   char line[MAX_LINE];
 
   while (fgets(line, sizeof(line), file) && todo_count < MAX_TODOS) {
-    if (line[0] == '[' && (line[1] == ' ' || line[1] == 'x')) {
-      todos[todo_count].done = (line[1] == 'x') ? 1 : 0;
-      strncpy(todos[todo_count].text, line + 4, MAX_LINE);
-      todos[todo_count].text[strcspn(todos[todo_count].text, "\n")] = 0;
-      todo_count++;
-    }
+    if(line[0] != '[') continue;
+    
+    Todo t;
+    t.done = (line[1] == 'x') ? 1 : 0;
+
+    char *prio_str = strtok(line + 4, "|");
+    char *deadline = strtok(NULL, "|");
+    char *text = strtok(NULL,"\n");
+
+    t.prio = atoi(prio_str);
+    strncpy(t.deadline, deadline ? deadline : "", sizeof(t.deadline));
+    strncpy(t.text, text ? text : "", MAX_LINE);
+
+    todos[todo_count++] = t;
   }
   fclose(file);
   return todo_count;
@@ -39,8 +47,12 @@ void save_todos(Todo todos[], int todo_count) {
     return;
   }
   for (int todo = 0; todo < todo_count; todo++){
-    fprintf(file, "[%c] %s\n", todos[todo].done ? 'x' : ' ',
-          todos[todo].text);
+    fprintf(file, "[%c]|%d|%s|%s\n",
+            todos[todo].done ? 'x' : ' ',
+            todos[todo].prio,
+            strlen(todos[todo].deadline) >  0 ? todos[todo].deadline : "          ",
+            todos[todo].text
+            );
   }
   fclose(file);
 }
@@ -50,10 +62,16 @@ void list_todos(Todo todos[], int todo_count) {
     printf("No todos yet!\n");
     return;
   }
-
-  for (int todo = 0; todo < todo_count; todo++){
-    printf("%2d. [%c] %s\n", todo, todos[todo].done ? 'x' : ' ',
-           todos[todo].text);
+  for (int todo = 0; todo < todo_count; todo++) {
+    printf("%2d. [%c] %s", todo, todos[todo].done ? 'x' : ' ', todos[todo].text);
+    if (todos[todo].prio > 0)
+      printf("  (Priority: %d", todos[todo].prio);
+    if (strcmp(todos[todo].deadline, "          ") != 0 && 
+        strlen(todos[todo].deadline) > 1)
+      printf(", Due: %s", todos[todo].deadline);
+    if (todos[todo].prio > 0 || strlen(todos[todo].deadline) > 1)
+      printf(")");
+    printf("\n");
   }
 }
 
@@ -62,9 +80,12 @@ void add_todo(Todo todos[], int *todo_count, const char *text){
     printf("Too many todos!\n");
     return;
   }
-  todos[*todo_count].done = 0;
-  strncpy(todos[*todo_count].text, text, MAX_LINE);
-  (*todo_count)++;
+  Todo t = {0};
+  t.done = 0;
+  t.prio = 2;
+  strcpy(t.deadline, "");
+  strncpy(t.text, text, MAX_LINE);
+  todos[(*todo_count)++] = t;
   save_todos(todos, *todo_count);
   printf("Added: %s\n", text);
 }
@@ -92,6 +113,38 @@ void delete_todo(Todo todos[], int *todo_count, int index){
   printf("Deleted todo #%d\n", index);
 }
 
+void edit_todo(Todo todos[], int todo_count, int index, const char *new_text) {
+  if (index < 0 || index >= todo_count){
+    printf("Invalid todo number.\n");
+    return;
+  }
+
+  strncpy(todos[index].text, new_text, MAX_LINE);
+  save_todos(todos, todo_count);
+  printf("Updated task #%d to: %s\n", index, new_text);
+}
+
+void set_deadline(Todo todos[], int todo_count, int index, const char *date) {
+  if (index < 0 || index >= todo_count){
+    printf("Invalid todo number.\n");
+    return;
+  }
+  strncpy(todos[index].deadline, date, 10+1);
+  save_todos(todos,todo_count);
+  printf("Set deadline for task #%d to: %s\n", index, date);
+}
+
+void set_priority(Todo todos[], int todo_count, int index, int prio) {
+  if (index < 0 || index >= todo_count || prio < 1 || prio > 3) {
+    printf("Invalid todo number or priority.\n");
+    return;
+  }
+
+  todos[todo_count].prio = prio;
+  save_todos(todos, todo_count);
+  printf("Set priority for task #%d to: %d\n", index, prio);
+}
+
 int main(int argc, char *argv[]) {
   Todo todos[MAX_TODOS];
   int todo_count = load_todos(todos);
@@ -102,6 +155,9 @@ int main(int argc, char *argv[]) {
     printf("  %s add \"task\"\n", argv[0]);
     printf("  %s done <number>\n", argv[0]);
     printf("  %s delete <number>\n", argv[0]);
+    printf("  %s edit <number> \"new text\"\n", argv[0]);
+    printf("  %s deadline <number> YYYY-MM-DD\n", argv[0]);
+    printf("  %s priority <number> <1|2|3>\n", argv[0]);
     return 1;
   }
 
@@ -113,6 +169,12 @@ int main(int argc, char *argv[]) {
     mark_done(todos, todo_count, atoi(argv[2]));
   } else if (strcmp(argv[1], "delete") == 0 && argc >= 3) {
     delete_todo(todos, &todo_count, atoi(argv[2]));
+  } else if (strcmp(argv[1], "edit") == 0 && argc >= 4) {
+    edit_todo(todos, todo_count, atoi(argv[2]), argv[3]);
+  } else if (strcmp(argv[1], "deadline") == 0 && argc >= 4) {
+    set_deadline(todos, todo_count, atoi(argv[2]), argv[3]);
+  } else if (strcmp(argv[1], "priority") == 0 && argc >= 4) {
+    set_priority(todos, todo_count, atoi(argv[2]), atoi(argv[3]));
   } else {
     printf("Unknown or incomplete command.\n");
   }
